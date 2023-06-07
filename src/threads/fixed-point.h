006ad4a42dc98f2b7555a40050546b82e3f72ff3
Mall0-w <kylelewis@rogers.com>
2023-06-07 15:40:08 -0400

    got fixed point working for real this time

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -5,27 +5,12 @@
 
 #define F 16384
 
-extern inline
-int int_to_fp (int n){
-    return n*F;
-}
+int int_to_fp (int n);
 
-extern inline
-int fp_to_int (int n){
-    int offset;
-    if (n < 0) offset = -(F/2);
-    else offset = F/2;
-    return (n + offset) / F;
-}
+int fp_to_int (int n);
 
-extern inline
-int multiply_fp(int x, int y){
-    return (int) (((int64_t) x) * y /F);
-}
+int fp_mult(int x, int y);
 
-extern inline
-int divide_fp(int x, int y){
-    return (int) (((int64_t) x) * F / y);
-}
+int fp_div(int x, int y);
 
 #endif /* threads/fixed-point.h */
