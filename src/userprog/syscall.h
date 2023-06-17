51e9336abcaff76c5c1f212c9fe64636dd2d6102
Mall0-w <kylelewis@rogers.com>
2023-06-17 17:01:24 -0400

    made make work

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,14 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include <stdint.h>
+#include <stdbool.h>
 
 void syscall_init (void);
+
+int sys_write(uint8_t* base_addr);
+int sys_exit(uint8_t* base_addr);
+
+int get_user (const uint8_t *uaddr);
+bool put_user (uint8_t *udst, uint8_t byte);
 
 #endif /* userprog/syscall.h */
